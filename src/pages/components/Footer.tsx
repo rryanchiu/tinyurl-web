15bfab79acbeed484a62816d7aa9831116da870a
Ryan <xiaoliang.q@outlook.com>
2025-05-15 00:39:28 +0800

    Update footer

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer text-gray-400">
             Made by <a className='mylink' target='_blank'
-                       href='https://rryan.me'>Ryan</a> | <a className='mylink'
+                       href='https://ryann.me'>Ryan</a> | <a className='mylink'
                                                                     target='_blank'
                                                                     href='https://github.com/rryanchiu/tinyurl'>Source
             Code</a>
